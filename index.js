4692d5f82aaf72a2c3393935fb4f15255c50e342
Maximilian Alba <albam476@gmail.com>
2022-06-06 22:14:10 -0400

    changes to index.html ID values, function for creating cards added to index.js

--- index.js
+++ index.js
@@ -1,16 +1,46 @@
-//Select form, add event listener for searching anime
-let form = document.querySelector('#searchAnime')
+// Select form, add event listener for searching anime
+let form = document.querySelector('#searchForm')
 form.addEventListener('submit', e => handleSearch(e))
 
+
 //Function that handles fetching anime from API
-function handleSearch(event){
-    event.preventDeafult()
-    let searchCriteria = document.querySelector('#searchCriteria')
+function handleSearch(e){
+    e.preventDefault()
+    console.log('form submit')
+    let searchCriteria = document.querySelector('#searchCriteria').value
     fetch(`https://api.jikan.moe/v4/anime?q=${searchCriteria}&sfw`).then(response => response.json()).then(data => createCard(data))
 }
 
-//Function that creates display cards for searched criteria
+// Function that creates display cards for searched criteria
 function createCard(data){
-    console.log(data)
+    let listOfSeries = data.data
+
+    //For each series in the array of series create elements for the card
+    listOfSeries.forEach(series => {
+        let list = document.querySelector('#generalList')
+        let card = document.createElement('div')
+        list.appendChild(card)
+        
+        //Create title node
+        let title = document.createElement('h1')
+        //Set title text = english title if english title exists
+        if (series.title_english !== null){
+            title.innerText = series.title_english
+            card.appendChild(title)
+        }
+
+        else{
+            title.innerText = series.title
+            card.appendChild(title)
+        }
+        //Give card unique ID of the series' title
+        card.setAttribute('id', `${title.innerText}`)
+    
+    
+
+
+
+
+    })
 
 }
